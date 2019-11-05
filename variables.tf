0bad84d714c36da524979bc6b68b088b90adb716
Dale-Kurt Murray <hello@dalekurtmurray.com>
2019-11-05 14:46:08 -0500

    Added terrform-null-label

--- variables.tf
+++ variables.tf
@@ -1,3 +1,38 @@
+variable "namespace" {
+  type        = string
+  description = "Namespace, which could be your organization name, e.g. 'eg' or 'cp'"
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
+  default     = ""
+}
+
+variable "name" {
+  type        = string
+  description = "Solution name, e.g. 'app' or 'cluster'"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit`,`XYZ`)"
+}
+
 variable "enabled" {
   description = "Whether to create the IAM user"
   default     = "true"
